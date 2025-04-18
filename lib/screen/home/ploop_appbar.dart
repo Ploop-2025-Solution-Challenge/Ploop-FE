4f4a8d74df2ebf15fa89b700d3a240b7f408485d
4ch1o3 <ljwon413@gmail.com>
2025-04-18 12:22:24 +0900

    fix: initialize profile while rendering appbar

---
+++ lib/screen/home/ploop_appbar.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../model/user_model.dart';
+import '../../service/user_service.dart';
+
+class PloopAppBar extends StatefulWidget {
+  PloopAppBar({
+    super.key,
+  });
+
+  @override
+  State<PloopAppBar> createState() => _PloopAppBarState();
+}
+
+class _PloopAppBarState extends State<PloopAppBar> {
+  late UserResponse userProfile =
+      UserResponse(id: -1, nickname: '', role: '', profileImageUrl: '');
+
+  Future<UserResponse?> _getUserProfile() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? jwt = prefs.getString('jwt');
+    if (jwt != null) {
+      final profile = await UserService.getUserProfile(jwt);
+      if (profile != null) {
+        setState(() {
+          userProfile = profile;
+        });
+      }
+    } else {
+      print('error: jwt is null');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserProfile();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // decoration: BoxDecoration(border: Border.all(color: Colors.green)),
+      width: MediaQuery.of(context).size.width,
+      // height: 90.h,
+      padding: EdgeInsets.fromLTRB(16.0.h, 10.0.h, 8.0.h, 8.0.h),
+      child: Column(
+        spacing: 24.h,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          DefaultTextStyle(
+            style: TextStyle(
+                fontFamily: 'PartialSansKR',
+                fontSize: 15.sp,
+                height: 0.5,
+                letterSpacing: 0.015.sp,
+                color: Colors.black),
+            child: const Text('Ploop'),
+          ),
+          // profile area
+          Row(
+            spacing: 9.w,
+            children: [
+              CircleAvatar(
+                backgroundImage: userProfile.profileImageUrl != ''
+                    ? NetworkImage(userProfile.profileImageUrl!)
+                    : null,
+                radius: 20.w,
+              ),
+              Text(
+                // TODO: get saved nickname from server
+                userProfile.nickname ?? 'User',
+                style: Theme.of(context).textTheme.labelMedium,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PloopTitleBar extends StatelessWidget {
+  const PloopTitleBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // decoration: BoxDecoration(border: Border.all(color: Colors.green)),
+      width: MediaQuery.of(context).size.width,
+      // height: 90.h,
+      padding: EdgeInsets.fromLTRB(16.0.h, 10.0.h, 8.0.h, 8.0.h),
+      child: Column(
+        spacing: 24.h,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          DefaultTextStyle(
+            style: TextStyle(
+                fontFamily: 'PartialSansKR',
+                fontSize: 15.sp,
+                height: 0.5,
+                letterSpacing: 0.015.sp,
+                color: Colors.black),
+            child: const Text('Ploop'),
+          ),
+          // profile area
+        ],
+      ),
+    );
+  }
+}
