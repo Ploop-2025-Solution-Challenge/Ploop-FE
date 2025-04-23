dab4c22a718998ffa0caffc9ead2a9befdbd79a0
4ch1o3 <ljwon413@gmail.com>
2025-04-23 11:08:55 +0900

    wip: create grpah with test data, no library

--- lib/screen/activity/dashboard_graph.dart
+++ lib/screen/activity/dashboard_graph.dart
@@ -1,18 +1,75 @@
 import 'package:dotted_line/dotted_line.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:graphic/graphic.dart';
 import 'package:ploop_fe/theme.dart';
 
-const testdata = [
-  {'day': 'monday', 'time': 2},
-  {'day': 'tuesday', 'time': 3},
-  {'day': 'wednesday', 'time': 5},
-  {'day': 'thursday', 'time': 2},
-  {'day': 'friday', 'time': 1},
-  {'day': 'saturday', 'time': 7},
-  {'day': 'sunday', 'time': 2},
-];
+class SingleBar {
+  static Widget makeBar(
+      double x, double y, String? label, TextStyle? labelStyle) {
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: theme().color_600,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(5.w),
+                topRight: Radius.circular(5.w),
+              ),
+            ),
+            width: x,
+            height: y,
+          ),
+          Text(label ?? '', style: labelStyle),
+        ],
+      ),
+    );
+  }
+}
+
+class GraphContainer extends StatelessWidget {
+  const GraphContainer({super.key});
+
+  double setWidth(int width, int spacing, int barCount) {
+    return width - spacing * (barCount - 1);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Map<String, double>> testData = [
+      {'Mon': 40},
+      {'Tue': 60},
+      {'Wed': 21},
+      {'Thr': 120},
+      {'Fri': 21},
+      {'Sat': 160},
+      {'Sun': 80}
+    ];
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        spacing: 16.w,
+        children: [
+          ...testData.map(
+            (e) {
+              String day = e.keys.first;
+              double value = e[day]!;
+              return SingleBar.makeBar(
+                  setWidth(348, 16, testData.length),
+                  value,
+                  day,
+                  Theme.of(context)
+                      .textTheme
+                      .bodySmall
+                      ?.copyWith(fontWeight: FontWeight.w600));
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 class GraphField extends StatelessWidget {
   final (DateTime, DateTime) dateRange;
@@ -25,7 +82,7 @@ class GraphField extends StatelessWidget {
       color: Colors.yellow,
       padding: EdgeInsets.only(top: 8.h),
       width: 370.w,
-      height: 266.h,
+      height: 226.h,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -44,36 +101,49 @@ class GraphField extends StatelessWidget {
                   GraphLine(),
                 ],
               ),
-              Positioned.fill(
+              Positioned(
                 bottom: 0,
                 child: Container(
-                  color: Colors.amber,
-                  height: 200.h,
+                  color: Colors.orangeAccent,
                   width: 348.w,
-                  child: Chart(
-                    data: testdata,
-                    variables: {
-                      'day': Variable(
-                        accessor: (Map map) => map['day'] as String,
-                      ),
-                      'time':
-                          Variable(accessor: (Map map) => map['time'] as int),
-                    },
-                    marks: [
-                      IntervalMark(
-                        color: ColorEncode(value: theme().color_600),
-                        size: SizeEncode(value: 36.w),
-                      ),
-                    ],
-                    coord: RectCoord(
-                      horizontalRange: [0, 312 / 348],
-                    ),
-                  ),
+                  height: 200.h,
+                  child:
+
+                      // child: BarChart(
+                      //   BarChartData(
+                      //     maxY: 160.h,
+                      //     minY: 0,
+                      //     gridData:
+                      //         FlGridData(show: true, horizontalInterval: 200.h / 5),
+                      //     titlesData: FlTitlesData(
+                      //       show: false,
+                      //     ),
+                      //     barGroups: test.barData
+                      //         .map(
+                      //           (data) => BarChartGroupData(
+                      //             x: data.x,
+                      //             barRods: [
+                      //               BarChartRodData(
+                      //                 toY: data.y,
+                      //                 color: theme().color_600,
+                      //                 width: 36.w,
+                      //                 borderRadius: BorderRadius.only(
+                      //                   topLeft: Radius.circular(5.w),
+                      //                   topRight: Radius.circular(5.w),
+                      //                 ),
+                      //               ),
+                      //             ],
+                      //           ),
+                      //         )
+                      //         .toList(),
+                      //   ),
+                      // ),
+                      GraphContainer(),
                 ),
               ),
             ],
           ),
-          DateRangeUnit(dateRange: dateRange),
+          // DateRangeUnit(dateRange: dateRange),
         ],
       ),
     );
